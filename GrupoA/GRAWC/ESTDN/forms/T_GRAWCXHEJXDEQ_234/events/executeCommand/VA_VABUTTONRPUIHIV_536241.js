bca5a6e0de3f6e882bf1d0e5f7b83346bfdea47c
jonathan borbor <jonathanbor2020@gmail.com>
2021-08-10 22:27:14 -0500

    Form jonathan

---
+++ GrupoA/GRAWC/ESTDN/forms/T_GRAWCXHEJXDEQ_234/events/executeCommand/VA_VABUTTONRPUIHIV_536241.js
@@ -0,0 +1,12 @@
+
+
+//Entity: Estudiante
+//Estudiante. (Button) View: EstudianteJibyForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONRPUIHIV_536241 = function(  entities, executeCommandEventArgs ) {
+
+    console.log(entities.Estudiante.nombre + " " + entities.Estudiante.apellido)
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.Estudiante = true;
+
+};
